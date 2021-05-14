3d4904c4b4fb4a24032c6abd442415be64393d2a
lalalalz <lalalalz@naver.com>
2021-05-14 16:09:40 +0900

    20210514 로그인, 회원가입, 사용자 테이블 변경

--- ram/router/index.js
+++ ram/router/index.js
@@ -5,10 +5,10 @@ const router  = express.Router();
  * @abstract
  * @description
  */
-router.post("/login", /*require("./login/recaptcha"),*/ require("./login/login"));
+router.post("/login", require("./login/recaptcha"), require("./login/login"));
 router.post("/auth",  require("./auth/auth"));
-router.post("/api/*", /*require("./api/hmac"),*/ require("./api/authority"), require("./api/api"));
-router.post("/pw/*", /*require("./api/hmac"),*/ require("./pw/pw"));
-router.post("/join", /*require("./api/hmac),*/ require("./join/join"));
+router.post("/api/*", require("./api/hmac"), require("./api/authority"), require("./api/api"));
+router.post("/pw/*", require("./api/hmac"), require("./pw/pw"));
+router.post("/join", /*require("./api/hmac"),*/ require("./join/join"));
 
 module.exports = router;
